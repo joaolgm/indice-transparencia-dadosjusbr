03f2f75dff6478a0995a3d4b584f9b94a14daca2
joaolgm <joao.menezes@ccc.ufcg.edu.br>
2021-09-14 10:45:10 -0300

    criação dos metadados para cálculo do índice

--- src/indice.py
+++ src/indice.py
@@ -1,29 +1,16 @@
 import pandas as pd
 
 
-def indice_overall(row, index, indice):
+def indice_overall(row, indice):
     # Lotação
     if row.workplace == True:
-        indice[index] += 1
+        indice["HasEmployeeWorkplace"] = 1
     # Cargo
     if row.role == True:
-        indice[index] += 1
+        indice["HasEmployeeRole"] = 1
     # Remuneração Básica
     if row.wage == True:
-        indice[index] += 1
-    # Detalhamento de Remuneração Eventual ou Temporária
-    if (
-        row.funds_eventual_benefits == True
-        or row.funds_personal_benefits == True
-        or row.funds_trust_position == True
-        or row.funds_gratification == True
-        or row.funds_daily == True
-        or row.funds_origin_pos == True
-        or row.funds_others_total == True
-    ) and row.funds_total == True:
-        indice[index] += 1
-    elif row.funds_total == True:
-        indice[index] += 0.5
+        indice["BaseRemuneration"] = 1
     # Detalhamento de Indenizações
     if (
         row.perks_food == True
@@ -40,35 +27,64 @@ def indice_overall(row, index, indice):
         or row.perks_furniture_transport == True
         or row.perks_premium_license_pecuniary == True
     ) and row.perks_total == True:
-        indice[index] += 1
+        indice["Benefits"] = 2
     elif row.perks_total == True:
-        indice[index] += 0.5
+        indice["Benefits"] = 1
     # Detalhamento de Descontos
     if (
         row.discount_prev_contribution == True
         or row.discounts_ceil_retention == True
         or row.discounts_income_tax == True
     ) and row.discounts_total == True:
-        indice[index] += 1
+        indice["Discounts"] = 2
     elif row.discounts_total == True:
-        indice[index] += 0.5
+        indice["Discounts"] = 1
 
     return indice
 
-# Mapeei meses em números para facilitar
-indice_tj = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
-indice_mp = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
 
-data_tj = pd.read_csv("../tjce-2020/data.csv")
+# Para efeito de teste, AGENCYID ainda se repete para todos os meses
+metaindice = {
+    "Year": 2020,
+    "Month": 1,
+    "AgencyID": "60620ad4a09c6aa6f8a3dd86",
+    "HasLogin": False,
+    "HasCaptcha": False,
+    "IsURLGood": False,
+    "OutputFormat": 2,
+    "DataAccess": 1,
+    "HasEmployeeRole": 0,
+    "HasEmployeeWorkplace": 0,
+    "BaseRemuneration": 0,
+    "Benefits": 0,
+    "Discounts": 0,
+}
+
+data_tj = pd.read_csv("./tjce-2020/data.csv")
 grouped_data_tj = data_tj.groupby(by=["month"]).first()
-data_mp = pd.read_csv("../mpce-2020/data.csv")
+data_mp = pd.read_csv("./mpce-2020/data.csv")
 grouped_data_mp = data_mp.groupby(by=["month"]).first()
 
-# Printa a o índice de transparência por mês
+
+# Utilizei 3 meses como exemplo
 for row in grouped_data_tj.notna().itertuples():
-    indice = indice_overall(row, row.Index, indice_tj)
-print("TJCE: \n", indice)
+    if row.Index == 3:
+        indice_mar = indice_overall(row, metaindice)
+        print("\n TJCE - MARÇO: \n", indice_mar)
+    elif row.Index == 4:
+        indice_abr = indice_overall(row, metaindice)
+        print("\n TJCE - ABRIL: \n", indice_abr)
+    elif row.Index == 5:
+        indice_mai = indice_overall(row, metaindice)
+        print("\n TJCE - MAIO: \n", indice_mai)
 
 for row in grouped_data_mp.notna().itertuples():
-    indice = indice_overall(row, row.Index, indice_mp)
-print("MPCE: \n", indice)
+    if row.Index == 3:
+        indice_mar = indice_overall(row, metaindice)
+        print("\n MPCE - MARÇO: \n", indice_mar)
+    elif row.Index == 4:
+        indice_abr = indice_overall(row, metaindice)
+        print("\n MPCE - ABRIL: \n", indice_abr)
+    elif row.Index == 5:
+        indice_mai = indice_overall(row, metaindice)
+        print("\n MPCE - MAIO: \n", indice_mai)
